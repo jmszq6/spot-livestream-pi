4fbbedb88923e98b0b77acee30d1a5d903c657a0
jmszq6 <70300105+jmszq6@users.noreply.github.com>
2021-05-14 19:24:34 -0500

    Update Client.java

--- spot-livestream-pi/src/mizzou/spot/cam/Client.java
+++ spot-livestream-pi/src/mizzou/spot/cam/Client.java
@@ -1,14 +1,53 @@
 package mizzou.spot.cam;
 
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+import mizzou.spot.connection.Connection;
+import mizzou.spot.connection.ConnectionType;
 
 public class Client
 {
-	private static final String SPOT_ADDRESS_WIRED = "10.0.0.4";
-	private static final String SPOT_ADDRESS_WIRELESS = "192.168.80.3";
-	private static final String SPOT_AUTH_SERVER_WIRED = SPOT_ADDRESS_WIRED + "/accounts/jwt/generate/";
-	private static final String SPOT_AUTH_SERVER_WIRELESS = SPOT_ADDRESS_WIRELESS + "/accounts/jwt/generate/";
-	private static final String SPOT_CAM_PORT = "31102";
-	private static final String SPOT_CAM_SERVER_WIRED = SPOT_ADDRESS_WIRED + ":" + SPOT_CAM_PORT + "/h264.sdp/";
-
-
+	//private static final String SPOT_CAM_PORT = "31102";
+	//private static final String SPOT_CAM_SERVER; //_WIRED = SPOT_ADDRESS_WIRED + ":" + SPOT_CAM_PORT + "/h264.sdp/";
+	
+	private static Connection connection;
+	private static String username;
+	private static String password;
+	
+	public static void main(String[] args)
+	{
+		if(args.length != 3)
+		{
+			System.err.println("Invalid number of arguments.\nRun with arguments (WIRED/WIRELESS username password).");
+			System.exit(1);
+		}
+		
+		final ConnectionType connectionType = args[0].equalsIgnoreCase("wired") ? ConnectionType.WIRED
+				: args[0].equalsIgnoreCase("wireless") ? ConnectionType.WIRELESS : null;
+		
+		if(connectionType == null) 
+		{
+			System.err.println("Unknown connection type. Use (WIRED/WIRELESS).");
+			System.exit(1);
+		}
+		
+		username = args[1];
+		password = args[2];
+		
+		try
+		{
+			connection = new Connection(connectionType, username, password);
+		} 
+		catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException | IOException e)
+		{
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		System.out.print("Connected with bearer token: ");
+		System.out.println(connection.getBearerToken());
+		
+	}
 }
