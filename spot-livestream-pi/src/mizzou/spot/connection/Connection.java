fd2baa89b4eb252e7a2bd3c1e6dd1622fab8e34b
jmszq6 <70300105+jmszq6@users.noreply.github.com>
2021-05-14 19:24:58 -0500

    Update Connection.java

--- spot-livestream-pi/src/mizzou/spot/connection/Connection.java
+++ spot-livestream-pi/src/mizzou/spot/connection/Connection.java
@@ -3,39 +3,32 @@ package mizzou.spot.connection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.net.ssl.SSLContext;
 
-import org.apache.http.HttpRequestFactory;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLContexts;
 import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.impl.DefaultHttpRequestFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.ssl.SSLContexts;
 
 public class Connection
 {
@@ -44,10 +37,24 @@ public class Connection
 	private final String SPOT_AUTH_SERVER = "/accounts/jwt/generate/";
 	
 	private final ConnectionType connectionType;
+	private final CloseableHttpClient httpClient;
+	private String bearerToken;
 	
-	public Connection(ConnectionType connectionType)
+	public Connection(ConnectionType connectionType, String username, String password) throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ClientProtocolException, IOException
 	{
+		// default trust strategy will be to trust all certificates, should change at some point to only the certs we need
+		TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
+		SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+		SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+
+		Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+				.register("https", sslsf).register("http", new PlainConnectionSocketFactory()).build();
+		
+		BasicHttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(socketFactoryRegistry);
+
 		this.connectionType = connectionType;
+		this.httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).setConnectionManager(connectionManager).build();
+		this.bearerToken = getNewBearerToken(username, password);
 	}
 	
 	public String getSpotAddress()
@@ -81,60 +88,50 @@ public class Connection
 	/**
 	 * Sends POST request with credentials to Spot authentication server and retrieves a new bearer token.
 	 * A Bearer token is required in request headers to verify access to Spot's resources.
+	 * Tokens expire after 12 hours but can be refreshed.
 	 *  
-	 *  @param username the name of the account you wish to authenticate as
-	 *  @param password the password for the account you wish to authenticate as
-	 *  @return a bearer token that gives access to Spot's resources
-	 * @throws InterruptedException 
+	 * @param username the name of the account you wish to authenticate as
+	 * @param password the password for the account you wish to authenticate as
+	 * @return a bearer token that gives access to Spot's resources
 	 * @throws IOException 
-	 * @throws KeyStoreException 
-	 * @throws NoSuchAlgorithmException 
-	 * @throws KeyManagementException 
 	 * @throws ClientProtocolException 
 	 */
-	public String getBearerToken(final String username, final String password) throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, ClientProtocolException, IOException 
+	protected String getNewBearerToken(final String username, final String password) throws ClientProtocolException, IOException
 	{
 		if (username == null || password == null)
 		{
 			return null;
 		}
-					
-		TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
-		SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
-		SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
 		
-		Registry<ConnectionSocketFactory> socketFactoryRegistry =
-				RegistryBuilder.<ConnectionSocketFactory>create().register("https", sslsf).register("http", new PlainConnectionSocketFactory()).build();
-		
-		BasicHttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(socketFactoryRegistry);
-		
-		CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).setConnectionManager(connectionManager).build();
-		
-	
-		
+		// build a post request to authentication server with username and password body
 		HttpPost request = new HttpPost(getSpotAuthServer());
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
 		params.add(new BasicNameValuePair("username", username));
 		params.add(new BasicNameValuePair("password", password));
 		request.setEntity(new UrlEncodedFormEntity(params));
 		
+		// execute the post request
 		CloseableHttpResponse response = httpClient.execute(request);
 		
-		System.out.println(response.getEntity());
-		
+		// build the response into a string
 		StringBuilder sb = new StringBuilder();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()), 65728);
 		
-			BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()), 65728);
-			
-			String line = null;
-			
-			while((line = reader.readLine()) != null)
-			{
+		String line = null;
+		while((line = reader.readLine()) != null)
+		{
 				sb.append(line);
-			}
-					
+		}
 		
-		return sb.toString();	
+		response.close();
+		reader.close();
+					
+		return sb.toString(); // bearer token
+	}
+
+	public String getBearerToken()
+	{
+		return bearerToken;
 	}
 
 }
